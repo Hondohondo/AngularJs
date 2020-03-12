ec4c0aeeda0e9c3a510f4cac8d2264e407eae309
hondohondo <mnandi19@jcu.edu>
2020-03-12 16:57:22 -0400

    custom styles and events

--- app/js/controllers/CourseController.js
+++ app/js/controllers/CourseController.js
@@ -12,7 +12,42 @@ courseApp.controller('CourseController',
                 address: 'PluralSight Website',
                 where: 'AngularJS Path'
             },
-            imageUrl: 'img/angular.png'
+            imageUrl: 'img/angular.png',
+            sessions: [
+                {
+                    name: 'Directives Masterclass',
+                    creatorName: 'Bob Smith',
+                    duration: '1 hr',
+                    level: 'Advanced',
+                    abstract: 'In this session you will learn the ins and outs of directives!',
+                    upVoteCount: 0
+                },
+                {
+                    name: 'Scopes for fun and projects',
+                    creatorName: 'John Doe',
+                    duration: '30 mins',
+                    level: 'Introductory',
+                    abstract: 'This session will take a closer look at scopes!',
+                    upVoteCount: 0
+                },
+                {
+                    name: 'Well Behaved Controllers',
+                    creatorName: 'Ami Amigo',
+                    duration: '2 hours',
+                    level: 'Intermediate',
+                    abstract: 'Controllers are the beginning of everything Angular!',
+                    upVoteCount: 0
+                }
+            ]
         }
+
+        $scope.upVoteSession = function (session) {
+            session.upVoteCount++;
+        };
+
+        $scope.downVoteSession = function (session) {
+            session.upVoteCount--;
+        };
     }
+
 );
